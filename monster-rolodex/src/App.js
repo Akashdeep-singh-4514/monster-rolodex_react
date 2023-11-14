b4d3e6b60296498c142e164b019485373a8daf2e
Akashdeep-singh-4514 <waliaakash95@gmail.com>
2023-11-14 19:17:50 +0530

    fetching api and printing in console

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component{
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users").then(response=>console.log(response.json()))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
 }
-
 export default App;

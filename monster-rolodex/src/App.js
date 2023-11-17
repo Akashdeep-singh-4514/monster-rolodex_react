18d16637fa7c7da8415b8f51294c31290a560e8c
Akashdeep-singh-4514 <waliaakash95@gmail.com>
2023-11-17 21:25:18 +0530

    finished styling

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -23,6 +23,7 @@ class App extends Component{
     const filteredMonsters=monsters.filter(monster=>monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
+        <h1>Monster Rolodex</h1>
         <SearchBox placeholder="search monster" handlechange={e=>{this.setState({searchField:e.target.value})}}/>
         <CardList monsters={filteredMonsters} />
       </div>
--- monster-rolodex/src/components/card/card-component.jsx
+++ monster-rolodex/src/components/card/card-component.jsx
@@ -5,7 +5,7 @@ export const Card=(props)=>{
 
     return(<div className='card-container'>
         <img alt={props.monster.id} src={`https://robohash.org/${props.monster.id}?set=2/`} height={200 }width={200}></img>
-        <h1>{props.monster.name}</h1>
+        <h2>{props.monster.name}</h2>
         <h5>{props.monster.email}</h5>
         </div>);
 }
